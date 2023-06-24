e007a97590c68828a9dec91e3eff529f9867209e
nh325 <104600815+nh325@users.noreply.github.com>
2023-06-23 21:21:33 -0400

    Update ad5933-test-threshold.ino

--- ad5933-test/ad5933-test-threshold.ino
+++ ad5933-test/ad5933-test-threshold.ino
@@ -2,133 +2,104 @@
 #include "AD5933.h"
 #include <Adafruit_MCP4725.h>
 
-// Constants
 #define START_FREQ  (10000)
 #define FREQ_INCR   (5000)
 #define NUM_INCR    (18)
 #define REF_RESIST  (300)
-#define IMPEDANCE_THRESHOLD 100
+#define THRESHOLD   (100)  // Impedance threshold value
 
-// Mux setup
-int Signal = 5;
-int sL[3] = {8, 9, 10};
-int MUXtable[8][3] = {
+int muxChannels[8][3] = {
   {0, 0, 0}, {1, 0, 0}, {0, 1, 0}, {1, 1, 0},
   {0, 0, 1}, {1, 0, 1}, {0, 1, 1}, {1, 1, 1}
 };
 
-// DAC setup for Pressure regulator
+int lastChannel = 0;  // Last channel that had a full frequency sweep
+
 Adafruit_MCP4725 dac;
-double Curvoltage = 0.5;
+double curVoltage = 0.5;
 
-// Pressure variables for barometric sensor
-const int pressureInput = A1;
-const int pressureZ = 102;
-const int pressureM = 921;
-float pressure;
-int normalP = 0;
+void setup() {
+  pinMode(Signal, OUTPUT);  // Set the Signal pin as an output
+  for (int i = 0; i < 3; i++)
+    pinMode(sL[i], OUTPUT);  // Set the sL pins as outputs
 
-void setup(void) {
-  // Mux setup
-  pinMode(Signal, OUTPUT);
-  for (int i = 0; i < 3; i++) {
-    pinMode(sL[i], OUTPUT);
-  }
-
-  // DAC setup (pressure regulator)
   pinMode(A2, OUTPUT);
-  digitalWrite(A2, LOW);
-  dac.begin(0x60);
+  digitalWrite(A2, LOW);  // Set A2 pin as an output and set it low
+  dac.begin(0x60);  // Initialize the MCP4725 DAC with address 0x60
 
-  // Begin serial at 9600 baud for output
-  Serial.begin(9600);
-
-  // Begin I2C
-  Wire.begin();
+  Serial.begin(9600);  // Initialize serial communication
+  Wire.begin();  // Initialize I2C communication
 
   Serial.println("AD5933 Test Started!");
 
-  // Perform initial configuration
   if (!(AD5933::reset() &&
         AD5933::setInternalClock(true) &&
         AD5933::setStartFrequency(START_FREQ) &&
         AD5933::setIncrementFrequency(FREQ_INCR) &&
         AD5933::setNumberIncrements(NUM_INCR) &&
-        AD5933::setPGAGain(PGA_GAIN_X1)))
-  {
+        AD5933::setPGAGain(PGA_GAIN_X1))) {
     Serial.println("FAILED in initialization!");
-    while (true) ;
+    while (true) ;  // If initialization fails, enter an infinite loop
   }
 
-  // Set mux to the calibration output (select Y0)
-  digitalWrite(sL[0], MUXtable[0][0]);
-  digitalWrite(sL[1], MUXtable[0][1]);
-  digitalWrite(sL[2], MUXtable[0][2]);
+  digitalWrite(sL[0], muxChannels[0][0]);  // Set the sL pins according to the first channel
+  digitalWrite(sL[1], muxChannels[0][1]);
+  digitalWrite(sL[2], muxChannels[0][2]);
 
-  // Perform calibration sweep
   if (AD5933::calibrate(gain, phase, REF_RESIST, NUM_INCR + 1))
-    Serial.println("Calibrated!");
+    Serial.println("Calibrated!");  
   else
-    Serial.println("Calibration failed...");
+    Serial.println("Calibration failed...");  
 }
 
-void loop(void) {
-  dac.setVoltage((Curvoltage * 4095) / 5, false);
-
-  for (int i = 0; i < 4; i++) {
-    selection(i);
+void loop() {
+  dac.setVoltage((curVoltage * 4095) / 5, false);  // Set the DAC voltage based on curVoltage
+  
+  for (int channel = lastChannel; channel < 9; channel++) {
+    lastChannel = selection(channel);  // Call the selection() function for each channel
   }
 
-  Curvoltage += 0.5;
-  if (Curvoltage > 2.9) {
-    Curvoltage = 0.5;
+  Curvoltage = Curvoltage + .5; // Increment the voltage counter
+  if (curVoltage > 2.9) {
+    curVoltage = 0.5;  // Reset the voltage counter if it exceeds 2.9
   }
 }
 
-void frequencySweepEasy(int pin) {
+void frequencySweepEasy(int channel) {
   int real[NUM_INCR + 1], imag[NUM_INCR + 1];
-  bool impedanceReachedThreshold = false;
-
+  
   if (AD5933::frequencySweep(real, imag, NUM_INCR + 1)) {
     int cfreq = START_FREQ / 1000;
-
-    Serial.print(pin);
+    Serial.print(channel);
     Serial.print(", ");
     Serial.print(normalP, 1);
 
     for (int i = 0; i < NUM_INCR + 1; i++, cfreq += FREQ_INCR / 1000) {
-      double magnitude = sqrt(pow(real[i], 2) + pow(imag[i], 2));
-      double impedance = 1 / (magnitude * gain[i]);
-
-      if (impedance < IMPEDANCE_THRESHOLD) {
-        impedanceReachedThreshold = true;
-        continue;
-      }
-
-      if (impedanceReachedThreshold) {
-        Serial.print(", ");
-        continue;
-      }
+      // Calculate and print out the pressure information
+      pressure = analogRead(pressureInput);
+      pressure = (pressure - pressureZ) * 15 / (pressureM - pressureZ);
+      normalP = abs((pressure * 6.89476) - 98);
 
       Serial.print(", ");
+      double magnitude = sqrt(pow(real[i], 2) + pow(imag[i], 2));
+      double impedance = 1 / (magnitude * gain[i]);
       Serial.print(impedance);
+
+      if (impedance > THRESHOLD) {
+        lastChannel = channel;  // Update lastChannel if impedance exceeds the threshold
+        Serial.print("Impedance reached Threshold at channel ");
+        Serial.print(lastChannel);
+        return lastChannel;  // Return the updated lastChannel value
+      }
     }
     Serial.println(" ");
-  } else {
-    // Frequency sweep failed
   }
 }
 
-void selection(int pin) {
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(Signal, LOW);
-    digitalWrite(sL[0], MUXtable[i][0]);
-    digitalWrite(sL[1], MUXtable[i][1]);
-    digitalWrite(sL[2], MUXtable[i][2]);
-    delay(1000);
-
-    pressure = analogRead(pressureInput);
-    normalP = map(pressure, pressureZ, pressureM, 0, 100);
-    frequencySweepEasy(pin);
-  }
+void selection(int channel) {
+  digitalWrite(sL[0], muxChannels[channel][0]);  // Set the sL pins based on the selected channel
+  digitalWrite(sL[1], muxChannels[channel][1]);
+  digitalWrite(sL[2], muxChannels[channel][2]);
+  lastChannel = frequencySweepEasy(channel);  // Call the frequencySweepEasy() function with the selected channel
+  return lastChannel;  // Return the updated lastChannel value
 }
